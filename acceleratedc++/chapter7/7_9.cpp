502f08b4b0623827999bf375e173a5f42f76d113
Q <qzqsmile@gmail.com>
2014-01-01 21:14:56 +0800

    7.9

--- acceleratedc++/chapter7/7_9.cpp
+++ acceleratedc++/chapter7/7_9.cpp
@@ -27,12 +27,13 @@ int main()
 
 int nrand(int n)
 {
-    if (n <= 0 || n >  RAND_MAX)
-        throw domain_error("Argument to nrand is out of rantge");
+    if (n <= 0 || n > RAND_MAX)
+	throw domain_error("Argument to nrand is out of rantge");
 
     const int bucket_size = RAND_MAX / n;
     int r;
-    do r = rand() / bucket_size;
+    do
+	r = rand() / bucket_size;
     while (r >= n);
 
     return r;
@@ -40,26 +41,57 @@ int nrand(int n)
 
 long int lnrand(long int n)
 {
-    int temp = 0;
+    int temp_rand = 0;
+    int four_bit_low = 0, four_bit_middle = 0, four_bit_high = 0;
     long int result = 0;
 
-    if (n < 32767){
+    if (n < 32767) {
         nrand(n);
-    }else{
-        if (bit_long_int(n) > 8){
-            temp = n % 10000 + 1; 
-            result = four_bit_rand(temp);
-            n = n / 10000;
-            temp = n % 10000 + 1;
-            result += four_bit_rand(temp) * 10000;
-            n = n / 10000;
-            temp = n % 10000 + 1;
-            result += four_bit_rand(temp) * 100000000;
-        }else{
-            temp = n % 10000 + 1;
-            result = four_bit_rand(temp);
-            temp = n / 10000 + 1;   
-            result = result + four_bit_rand (temp) * 10000;
+    } else {
+        if (bit_long_int(n) > 8) {
+
+            four_bit_high = n / 100000000;
+            four_bit_low = n % 10000;
+            four_bit_middle = (n / 10000) % 10000;
+
+            temp_rand = nrand((four_bit_high) + 1);
+            if (temp_rand < four_bit_high) {
+                result +=
+                    10000 * 10000 * temp_rand + nrand(10000) +
+                    nrand(10000);
+            } else if( temp_rand == four_bit_high ){
+                temp_rand = nrand((four_bit_middle + 1));
+                if (temp_rand < four_bit_middle) {
+                    result += four_bit_high * 10000 * 10000 + temp_rand * 10000 + nrand(10000);
+                } else if(four_bit_middle == temp_rand){
+                    temp_rand = nrand((four_bit_low + 1)); 
+                    if (temp_rand <= four_bit_low) {
+                        result += four_bit_high * 10000 * 10000 + four_bit_middle * 10000 + temp_rand;
+                    }else{
+                        result = lnrand(n);
+                    }
+                }else{
+                    result = lnrand(n);
+                }
+            }else {
+                result = lnrand(n);
+            }
+        } else {
+            four_bit_low = n % 10000;
+            four_bit_middle = n / 10000;
+            temp_rand = nrand((four_bit_high)+1);
+            if(temp_rand < four_bit_high){
+                result += temp_rand * 10000 * 10000 + nrand(10000);
+            }else if(temp_rand == four_bit_high){
+                temp_rand = nrand((four_bit_low + 1));
+                if (temp_rand <= four_bit_low){
+                    result += four_bit_middle * 10000 + nrand(10000);
+                }else{
+                    result += lnrand(n);
+                }
+            }else{
+                result = lnrand(n);
+            }
         }
     }
     return result;
@@ -69,18 +101,17 @@ int bit_long_int(long int n)
 {
     int bit = 1;
 
-    while (n = n / 10){
+    while (n = n / 10) {
         bit++;
     }
-    
+
     return bit;
 }
 
 int four_bit_rand(int n)
 {
     if (1 == n)
-        return nrand(10000);
-    else 
-        return nrand(n);
+	return nrand(10000);
+    else
+	return nrand(n);
 }
-
