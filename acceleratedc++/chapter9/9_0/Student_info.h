1d20697757c275994202345c0bb31307a2a9654f
Q_Q <qzqsmile@gmail.com>
2014-01-31 17:03:59 +0800

    e

---
+++ acceleratedc++/chapter9/9_0/Student_info.h
@@ -0,0 +1,23 @@
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+class Student_info{
+    public:
+	std::string name() const {return n;}
+	bool valid() const{return !homework.empty();}
+	std::istream& read(std::istream&);
+	double grade() const;
+    private:
+	std::string n;
+	double midterm, final;
+	std::vector<double> homework;
+};
+
+bool compare(const Student_info&, const Student_info&);
+std::istream& read_hw(std::istream&, std::vector<double>&);
+
+#endif
