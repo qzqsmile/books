0532722f82bd5ef07ac32549052e04c504a9741a
Q_Q <qzqsmile@gmail.com>
2014-02-16 18:35:44 +0800

    new chapter

---
+++ linux_c/chapter18/18_6.c
@@ -0,0 +1,16 @@
+volatile unsigned char recv;
+volatile unsigned char send;
+
+unsigned char buf[3];
+
+int main(void)
+{
+	buf[0] = recv;
+	buf[1] = recv;
+	buf[2] = recv;
+	send = ~buf[0];
+	send = ~buf[1];
+	send = ~buf[2];
+
+	return 0;
+}
---
+++ linux_c/chapter18/main.s
@@ -0,0 +1,72 @@
+	.file	"main.c"
+	.text
+	.globl	bar
+	.type	bar, @function
+bar:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	12(%ebp), %eax
+	movl	8(%ebp), %edx
+	addl	%edx, %eax
+	movl	%eax, -4(%ebp)
+	movl	-4(%ebp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	bar, .-bar
+	.globl	foo
+	.type	foo, @function
+foo:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$8, %esp
+	movl	12(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	8(%ebp), %eax
+	movl	%eax, (%esp)
+	call	bar
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	foo, .-foo
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$8, %esp
+	movl	$3, 4(%esp)
+	movl	$2, (%esp)
+	call	foo
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 4.7.3-1ubuntu1) 4.7.3"
+	.section	.note.GNU-stack,"",@progbits
