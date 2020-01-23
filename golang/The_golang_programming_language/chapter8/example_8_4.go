4274a79329f7704eb336e2b070f849f420ee0eaa
Q Q <qq@chows-x.citrite.net>
2020-01-23 15:17:37 +0800

    add new demo code

--- golang/The_golang_programming_language/chapter8/example_8_4.go
+++ golang/The_golang_programming_language/chapter8/example_8_4.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -12,11 +14,71 @@ func demo84() {
 		log.Fatal(err)
 	}
 	done := make(chan struct{})
-	//go func() {
-	//	io.Copy(os.Stdout, conn) // NOTE: ignoring errors log.Println("done")
-	//	done <- struct{}{}       // signal the main goroutine
-	//}()
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors log.Println("done")
+		done <- struct{}{}       // signal the main goroutine
+	}()
 	mustCopy(conn, os.Stdin)
 	conn.Close()
 	<-done // wait for background goroutine to finish
+}
+
+
+func demo842(){
+	naturals := make(chan int)
+	squares := make(chan int)
+	// Counter
+	go func() {
+		for x := 0; x < 100 ; x++ {
+			naturals <- x
+		}
+		close(naturals)
+		}()
+	// Squarer
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+	// Printer (in main goroutine)
+	for x := range squares {
+		fmt.Println(x)
+	}
+}
+
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
+func demo843(){
+	naturals := make(chan int)
+	squares := make(chan int)
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
+
+}
+
+func mydemo(){
+	s := make(chan int)
+	go func(){
+		close(s)
+	}()
+
+	fmt.Println(<-s)
 }
--- golang/The_golang_programming_language/chapter8/main.go
+++ golang/The_golang_programming_language/chapter8/main.go
@@ -2,7 +2,10 @@ package main
 
 func main() {
 	//demo81()
-	go demo82()
+	//go demo82()
 	//demo822()
-	demo84()
+	//demo84()
+	//demo842()
+	//mydemo()
+	demo843()
 }
